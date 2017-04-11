221cfa97c54c3c7d6b4f90d8e327a31b97d9e56a
藤田 和樹 <fujitakazuki@imac.local>
2017-04-12 06:36:14 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 藤田 和樹 on 2017/04/12.
+//  Copyright © 2017年 藤田 和樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
