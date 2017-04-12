c9d143372642f728ce15760eab9e8c7104b611d3
藤田 和樹 <fujitakazuki@imac.local>
2017-04-13 03:14:06 +0900

    画像表示設定

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -13,10 +13,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func onNext(_ sender: Any) {
+        dispImageNo += 1
+        displayImage()
     }
     @IBAction func onPrev(_ sender: Any) {
+        dispImageNo -= 1
+        displayImage()
     }
+    var dispImageNo = 0
     
+    func displayImage() {
+        
+        let imageNameArray = ["01.jpg","02.jpg","03.jpg",]
+    
+        let name = imageNameArray[dispImageNo]
+    
+        let image = UIImage(named: name)
+        
+        imageView.image = image
+    }
     @IBAction func onTspImage(_ sender: Any) {
         performSegue(withIdentifier: "result", sender: nil)
     }
